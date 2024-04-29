efc90c790cf3f0c089a548abb6ad3a0206a926a6
Olivier Stankiewicz <olivierst03@gmail.com>
2024-04-29 21:47:21 +0200

    changed the message in app.py and test.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 @app.route("/")
 #hello function
 def hello():
-   return "Hello CICD!"
+   return "Hello Olivier!"
 
 if __name__ == "__main__":
    app.run(host='0.0.0.0', port=8080)
--- test.py
+++ test.py
@@ -12,4 +12,4 @@ def client():
 def test_app_is_working(client):
     response = client.get('/')
     assert response.status_code == 200
-    assert b"Hello CICD!" in response.data
+    assert b"Hello Olivier!" in response.data
